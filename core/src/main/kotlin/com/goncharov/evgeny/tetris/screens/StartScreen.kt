bcb8b3f3054acc270d7f91e6551aeb791819cad6
Evgeny Goncharov <jtgn@yandex.ru>
2023-07-15 18:43:28 +0300

    added empty method

--- core/src/main/kotlin/com/goncharov/evgeny/tetris/screens/StartScreen.kt
+++ core/src/main/kotlin/com/goncharov/evgeny/tetris/screens/StartScreen.kt
@@ -4,4 +4,23 @@ import com.badlogic.gdx.ScreenAdapter
 
 class StartScreen : ScreenAdapter() {
 
+    override fun show() {
+
+    }
+
+    override fun render(delta: Float) {
+
+    }
+
+    override fun resize(width: Int, height: Int) {
+
+    }
+
+    override fun hide() {
+        dispose()
+    }
+
+    override fun dispose() {
+
+    }
 }
