b2313a77a74b8b9b67c13eb57983059696f5c9e7
Evgeny Goncharov <jtgn@yandex.ru>
2023-07-16 14:33:21 +0300

    start koin and add KoinLogger

--- core/src/main/kotlin/com/goncharov/evgeny/tetris/CoreApp.kt
+++ core/src/main/kotlin/com/goncharov/evgeny/tetris/CoreApp.kt
@@ -2,10 +2,19 @@ package com.goncharov.evgeny.tetris
 
 import com.badlogic.gdx.Game
 import com.goncharov.evgeny.tetris.screens.StartScreen
+import com.goncharov.evgeny.tetris.utils.KoinLogger
+import org.koin.core.context.startKoin
+
 
 class CoreApp : Game() {
 
     override fun create() {
         setScreen(StartScreen())
+    }
+
+    private fun initKoin() {
+        startKoin {
+            logger(KoinLogger())
+        }
     }
 }
