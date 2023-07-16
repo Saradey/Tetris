bb76868c386a868195d70f8d44e6a3ad16caaf1b
Evgeny Goncharov <jtgn@yandex.ru>
2023-07-16 14:53:34 +0300

    added config consts

---
+++ core/src/main/kotlin/com/goncharov/evgeny/tetris/utils/GameConfig.kt
@@ -0,0 +1,5 @@
+package com.goncharov.evgeny.tetris.utils
+
+const val WINDOW_DESKTOP_WIDTH_SIZE = 601
+const val WINDOW_DESKTOP_HEIGHT_SIZE = 601
+
--- lwjgl3/src/main/kotlin/com/goncharov/evgeny/tetris/lwjgl3/Lwjgl3Launcher.kt
+++ lwjgl3/src/main/kotlin/com/goncharov/evgeny/tetris/lwjgl3/Lwjgl3Launcher.kt
@@ -5,6 +5,8 @@ package com.goncharov.evgeny.tetris.lwjgl3
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
 import com.goncharov.evgeny.tetris.CoreApp
+import com.goncharov.evgeny.tetris.utils.WINDOW_DESKTOP_HEIGHT_SIZE
+import com.goncharov.evgeny.tetris.utils.WINDOW_DESKTOP_WIDTH_SIZE
 
 /** Launches the desktop (LWJGL3) application. */
 fun main() {
@@ -13,7 +15,7 @@ fun main() {
       return
     Lwjgl3Application(CoreApp(), Lwjgl3ApplicationConfiguration().apply {
         setTitle("Tetris")
-        setWindowedMode(640, 480)
+        setWindowedMode(WINDOW_DESKTOP_WIDTH_SIZE, WINDOW_DESKTOP_HEIGHT_SIZE)
         setWindowIcon(*(arrayOf(128, 64, 32, 16).map { "libgdx$it.png" }.toTypedArray()))
     })
 }
