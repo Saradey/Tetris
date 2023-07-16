6af5b5bc392a02c12aaf40e27ee58e7a84480c20
Evgeny Goncharov <jtgn@yandex.ru>
2023-07-16 14:47:08 +0300

    added GameScreen

---
+++ core/src/main/kotlin/com/goncharov/evgeny/tetris/screens/GameScreen.kt
@@ -0,0 +1,26 @@
+package com.goncharov.evgeny.tetris.screens
+
+import com.badlogic.gdx.ScreenAdapter
+
+class GameScreen : ScreenAdapter() {
+
+    override fun show() {
+
+    }
+
+    override fun render(delta: Float) {
+
+    }
+
+    override fun resize(width: Int, height: Int) {
+
+    }
+
+    override fun hide() {
+        dispose()
+    }
+
+    override fun dispose() {
+
+    }
+}
