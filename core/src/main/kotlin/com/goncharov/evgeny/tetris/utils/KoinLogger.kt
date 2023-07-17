e35c21dede48de8bf028ce1b42762ef4cb645de8
Evgeny Goncharov <jtgn@yandex.ru>
2023-07-17 23:50:45 +0300

    added logic to KoinLogger

--- core/src/main/kotlin/com/goncharov/evgeny/tetris/utils/KoinLogger.kt
+++ core/src/main/kotlin/com/goncharov/evgeny/tetris/utils/KoinLogger.kt
@@ -1,12 +1,19 @@
 package com.goncharov.evgeny.tetris.utils
 
+import com.badlogic.gdx.Gdx
 import org.koin.core.logger.Level
 import org.koin.core.logger.Logger
 import org.koin.core.logger.MESSAGE
 
-class KoinLogger : Logger(){
+class KoinLogger : Logger() {
 
     override fun display(level: Level, msg: MESSAGE) {
-        //TODO
+        if (level == Level.DEBUG) {
+            Gdx.app.debug(KOIN_TAG, msg)
+        }
+    }
+
+    private companion object {
+        const val KOIN_TAG = "KOIN_TAG"
     }
 }
