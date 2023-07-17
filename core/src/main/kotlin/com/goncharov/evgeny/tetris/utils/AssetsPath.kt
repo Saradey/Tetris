61e7c2d0cbd6f4fdde5eb036a15514503cf64583
Evgeny Goncharov <jtgn@yandex.ru>
2023-07-17 23:15:08 +0300

    added assets path

---
+++ core/src/main/kotlin/com/goncharov/evgeny/tetris/utils/AssetsPath.kt
@@ -0,0 +1,12 @@
+package com.goncharov.evgeny.tetris.utils
+
+//sprites
+const val GAME_ASSET_PATH = "texture_atlas/tetris_spite_sheet.atlas"
+const val UI_ASSET_PATH = "ui/Untitled.json"
+
+//sound
+const val SOUND_CLEAR_LINE_PATH = "sound/sound_clear_line.mp3"
+const val SOUND_CLICK_BUTTON_PATH = "sound/sound_click_button.mp3"
+const val SOUND_FALL_TO_FLOOR_PATH = "sound/sound_fall_to_floor.mp3"
+const val SOUND_GAME_OVER_PATH = "sound/sound_game_over.mp3"
+const val SOUND_TERN_FIGURE_PATH = "sound/sound_tern_figure.mp3"
