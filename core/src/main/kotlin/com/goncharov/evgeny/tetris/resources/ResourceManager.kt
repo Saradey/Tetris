1405733bd7258770d8881acf842348179555a9d3
Evgeny Goncharov <jtgn@yandex.ru>
2023-07-17 23:30:12 +0300

    loaded resource

--- core/src/main/kotlin/com/goncharov/evgeny/tetris/resources/ResourceManager.kt
+++ core/src/main/kotlin/com/goncharov/evgeny/tetris/resources/ResourceManager.kt
@@ -6,5 +6,14 @@ class ResourceManager(
     private val assetManager: AssetManager
 ) {
 
-
+    fun loadedAllResources() {
+        assetManager.load(GAME_ASSET_DESCRIPTOR)
+        assetManager.load(UI_ASSET_DESCRIPTOR)
+        assetManager.load(SOUND_CLEAR_LINE_DESCRIPTOR)
+        assetManager.load(SOUND_CLICK_BUTTON_DESCRIPTOR)
+        assetManager.load(SOUND_FALL_TO_FLOOR_DESCRIPTOR)
+        assetManager.load(SOUND_GAME_OVER_DESCRIPTOR)
+        assetManager.load(SOUND_TERN_FIGURE_DESCRIPTOR)
+        assetManager.finishLoading()
+    }
 }
