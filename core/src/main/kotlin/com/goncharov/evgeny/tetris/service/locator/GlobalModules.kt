cb4a9b349d3e46f013132161f1a6dc6c05515c87
Evgeny Goncharov <jtgn@yandex.ru>
2023-07-23 00:53:54 +0300

    added DifficultyRepository to koin

--- core/src/main/kotlin/com/goncharov/evgeny/tetris/service/locator/GlobalModules.kt
+++ core/src/main/kotlin/com/goncharov/evgeny/tetris/service/locator/GlobalModules.kt
@@ -3,10 +3,13 @@ package com.goncharov.evgeny.tetris.service.locator
 import com.badlogic.gdx.assets.AssetManager
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.goncharov.evgeny.tetris.navigation.Navigator
+import com.goncharov.evgeny.tetris.repositories.DifficultyRepository
+import com.goncharov.evgeny.tetris.repositories.DifficultyRepositoryImpl
 import com.goncharov.evgeny.tetris.resources.ResourceManager
 import com.goncharov.evgeny.tetris.screens.difficulty.service.locator.difficultyModule
 import com.goncharov.evgeny.tetris.screens.start.service.locator.startModule
 import org.koin.core.module.Module
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val gdxDependencyModule: Module = module {
@@ -25,6 +28,10 @@ fun createCoreModule(navigator: Navigator): Module = module {
     single {
         navigator
     }
+}
+
+val repositoriesModule: Module = module {
+    single { DifficultyRepositoryImpl() } bind DifficultyRepository::class
 }
 
 val allModules = listOf(
--- core/src/main/kotlin/com/goncharov/evgeny/tetris/service/locator/factorys/Factory.kt
+++ core/src/main/kotlin/com/goncharov/evgeny/tetris/service/locator/factorys/Factory.kt
@@ -1,5 +1,9 @@
 package com.goncharov.evgeny.tetris.service.locator.factorys
 
 import com.badlogic.gdx.utils.viewport.FitViewport
+import com.goncharov.evgeny.tetris.repositories.DifficultyRepository
+import com.goncharov.evgeny.tetris.repositories.DifficultyRepositoryImpl
 
 fun createViewPort(width: Float, height: Float): FitViewport = FitViewport(width, height)
+
+fun createDifficultyRepository() : DifficultyRepository = DifficultyRepositoryImpl()
