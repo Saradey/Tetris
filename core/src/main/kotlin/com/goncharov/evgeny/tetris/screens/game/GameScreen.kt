295c44641cf97f16c907b9e5f5ac0e3b53b80e2d
Evgeny Goncharov <jtgn@yandex.ru>
2023-07-23 01:07:20 +0300

    added logic in GameScreen

--- core/src/main/kotlin/com/goncharov/evgeny/tetris/screens/game/GameScreen.kt
+++ core/src/main/kotlin/com/goncharov/evgeny/tetris/screens/game/GameScreen.kt
@@ -1,26 +1,39 @@
 package com.goncharov.evgeny.tetris.screens.game
 
+import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.ScreenAdapter
+import com.badlogic.gdx.utils.viewport.FitViewport
+import com.goncharov.evgeny.tetris.utils.clearScreen
+import org.koin.core.component.KoinScopeComponent
+import org.koin.core.component.createScope
+import org.koin.core.component.inject
+import org.koin.core.scope.Scope
 
-class GameScreen : ScreenAdapter() {
+class GameScreen : ScreenAdapter(), KoinScopeComponent {
+
+    override val scope: Scope = createScope()
+    private val uiViewPort: FitViewport by inject()
 
     override fun show() {
 
     }
 
     override fun render(delta: Float) {
-
+        clearScreen()
     }
 
     override fun resize(width: Int, height: Int) {
+        uiViewPort.update(width, height, true)
+    }
 
+    override fun dispose() {
+        Gdx.input.inputProcessor = null
+        if (scope.isNotClosed()) {
+            scope.close()
+        }
     }
 
     override fun hide() {
         dispose()
-    }
-
-    override fun dispose() {
-
     }
 }
