106708fe20e3310755f792d09bee96c92fd841e6
Evgeny Goncharov <jtgn@yandex.ru>
2023-07-22 11:18:47 +0300

    added dependency to DifficultyScene

--- core/src/main/kotlin/com/goncharov/evgeny/tetris/screens/difficulty/service/locator/DifficultyFactory.kt
+++ core/src/main/kotlin/com/goncharov/evgeny/tetris/screens/difficulty/service/locator/DifficultyFactory.kt
@@ -1,5 +1,14 @@
 package com.goncharov.evgeny.tetris.screens.difficulty.service.locator
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.utils.viewport.Viewport
+import com.goncharov.evgeny.tetris.navigation.Navigator
+import com.goncharov.evgeny.tetris.resources.ResourceManager
 import com.goncharov.evgeny.tetris.screens.difficulty.ui.DifficultyScene
 
-fun createDifficultyScene() = DifficultyScene()
+fun createDifficultyScene(
+    viewport: Viewport,
+    spriteBatch: SpriteBatch,
+    resourceManager: ResourceManager,
+    navigator: Navigator
+) = DifficultyScene(viewport, spriteBatch, resourceManager, navigator)
--- core/src/main/kotlin/com/goncharov/evgeny/tetris/screens/difficulty/ui/DifficultyScene.kt
+++ core/src/main/kotlin/com/goncharov/evgeny/tetris/screens/difficulty/ui/DifficultyScene.kt
@@ -1,8 +1,30 @@
 package com.goncharov.evgeny.tetris.screens.difficulty.ui
 
+import com.badlogic.gdx.audio.Sound
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.ui.Skin
+import com.badlogic.gdx.utils.viewport.Viewport
+import com.goncharov.evgeny.tetris.navigation.Navigator
+import com.goncharov.evgeny.tetris.resources.ResourceManager
+import com.goncharov.evgeny.tetris.resources.SOUND_CLICK_BUTTON_DESCRIPTOR
+import com.goncharov.evgeny.tetris.resources.UI_ASSET_DESCRIPTOR
 
-class DifficultyScene : Stage() {
+class DifficultyScene(
+    viewport: Viewport,
+    spriteBatch: SpriteBatch,
+    resourceManager: ResourceManager,
+    private val navigator: Navigator
+) : Stage(viewport, spriteBatch) {
 
+    private val uiSkin: Skin = resourceManager[UI_ASSET_DESCRIPTOR]
+    private val soundClickButton: Sound = resourceManager[SOUND_CLICK_BUTTON_DESCRIPTOR]
 
+    init {
+        initUi()
+    }
+
+    private fun initUi() {
+
+    }
 }
