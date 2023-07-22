790c85222f4e2c139da5e70ac0ff8dd209a45949
Evgeny Goncharov <jtgn@yandex.ru>
2023-07-22 23:11:15 +0300

    added titleDot and titleDot

--- core/src/main/kotlin/com/goncharov/evgeny/tetris/screens/difficulty/ui/DifficultyScene.kt
+++ core/src/main/kotlin/com/goncharov/evgeny/tetris/screens/difficulty/ui/DifficultyScene.kt
@@ -3,6 +3,7 @@ package com.goncharov.evgeny.tetris.screens.difficulty.ui
 import com.badlogic.gdx.audio.Sound
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.ui.Image
 import com.badlogic.gdx.scenes.scene2d.ui.Skin
 import com.badlogic.gdx.scenes.scene2d.ui.Table
 import com.badlogic.gdx.utils.viewport.Viewport
@@ -11,7 +12,10 @@ import com.goncharov.evgeny.tetris.navigation.Navigator
 import com.goncharov.evgeny.tetris.resources.BACKGROUND_LINE_UI_PATH
 import com.goncharov.evgeny.tetris.resources.ResourceManager
 import com.goncharov.evgeny.tetris.resources.SOUND_CLICK_BUTTON_DESCRIPTOR
+import com.goncharov.evgeny.tetris.resources.TITLE_SHAPE_PATH
+import com.goncharov.evgeny.tetris.resources.TITLE_UI_PATH
 import com.goncharov.evgeny.tetris.resources.UI_ASSET_DESCRIPTOR
+import com.goncharov.evgeny.tetris.screens.start.ui.StartScene
 
 class DifficultyScene(
     viewport: Viewport,
@@ -23,6 +27,8 @@ class DifficultyScene(
     private val uiSkin: Skin = resourceManager[UI_ASSET_DESCRIPTOR]
     private val soundClickButton: Sound = resourceManager[SOUND_CLICK_BUTTON_DESCRIPTOR]
     private val root = Table()
+    private val titleDot = Image(uiSkin, TITLE_SHAPE_PATH)
+    private val title = Image(uiSkin, TITLE_UI_PATH)
 
     init {
         initUi()
@@ -31,6 +37,13 @@ class DifficultyScene(
     private fun initUi() {
         root.setFillParent(true)
         root.background(MainBackground(uiSkin.getSprite(BACKGROUND_LINE_UI_PATH)))
+        root.add(titleDot).padLeft(LEFT_MARGIN_TITLE_DOT).padBottom(MOVE_TITLE_DOT_POSITION).row()
+        root.add(title).expandX().row()
         addActor(root)
+    }
+
+    private companion object {
+        const val LEFT_MARGIN_TITLE_DOT = 84f
+        const val MOVE_TITLE_DOT_POSITION = -20f
     }
 }
