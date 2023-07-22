3f1ced4263f29f6260169f0786dd3c193926d3f0
Evgeny Goncharov <jtgn@yandex.ru>
2023-07-22 11:10:00 +0300

    added difficulty module to koin

--- core/src/main/kotlin/com/goncharov/evgeny/tetris/service/locator/GlobalModules.kt
+++ core/src/main/kotlin/com/goncharov/evgeny/tetris/service/locator/GlobalModules.kt
@@ -4,6 +4,7 @@ import com.badlogic.gdx.assets.AssetManager
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.goncharov.evgeny.tetris.navigation.Navigator
 import com.goncharov.evgeny.tetris.resources.ResourceManager
+import com.goncharov.evgeny.tetris.screens.difficulty.service.locator.difficultyModule
 import com.goncharov.evgeny.tetris.screens.start.service.locator.startModule
 import org.koin.core.module.Module
 import org.koin.dsl.module
@@ -28,5 +29,6 @@ fun createCoreModule(navigator: Navigator): Module = module {
 
 val allModules = listOf(
     gdxDependencyModule,
-    startModule
+    startModule,
+    difficultyModule
 )
