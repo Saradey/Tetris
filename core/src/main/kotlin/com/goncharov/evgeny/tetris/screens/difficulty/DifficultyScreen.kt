1e8113bc1fb6999ccd02cacec55ea0c41661dabf
Evgeny Goncharov <jtgn@yandex.ru>
2023-07-22 11:08:40 +0300

    added logic to DifficultyScreen

--- core/src/main/kotlin/com/goncharov/evgeny/tetris/screens/difficulty/DifficultyScreen.kt
+++ core/src/main/kotlin/com/goncharov/evgeny/tetris/screens/difficulty/DifficultyScreen.kt
@@ -1,6 +1,8 @@
 package com.goncharov.evgeny.tetris.screens.difficulty
 
+import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.ScreenAdapter
+import com.badlogic.gdx.utils.viewport.FitViewport
 import com.goncharov.evgeny.tetris.screens.difficulty.ui.DifficultyScene
 import com.goncharov.evgeny.tetris.utils.clearScreen
 import org.koin.core.component.KoinScopeComponent
@@ -12,24 +14,31 @@ class DifficultyScreen : ScreenAdapter(), KoinScopeComponent {
 
     override val scope: Scope = createScope()
     private val difficultyScene: DifficultyScene by inject()
+    private val uiViewPort: FitViewport by inject()
 
     override fun show() {
-        difficultyScene
+        Gdx.input.inputProcessor = difficultyScene
     }
 
     override fun render(delta: Float) {
         clearScreen()
+        difficultyScene.act()
+        difficultyScene.draw()
     }
 
     override fun resize(width: Int, height: Int) {
+        uiViewPort.update(width, height, true)
+    }
 
+    override fun dispose() {
+        Gdx.input.inputProcessor = null
+        difficultyScene.dispose()
+        if (scope.isNotClosed()) {
+            scope.close()
+        }
     }
 
     override fun hide() {
         dispose()
-    }
-
-    override fun dispose() {
-
     }
 }
