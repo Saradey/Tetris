868cb62eeaf565df1f3f86411922b8880e40bb7a
Evgeny Goncharov <jtgn@yandex.ru>
2023-07-23 16:08:29 +0300

    added backgroundSpawnInfo

--- core/src/main/kotlin/com/goncharov/evgeny/tetris/resources/UiPath.kt
+++ core/src/main/kotlin/com/goncharov/evgeny/tetris/resources/UiPath.kt
@@ -3,6 +3,7 @@ package com.goncharov.evgeny.tetris.resources
 const val BACKGROUND_LINE_UI_PATH = "background"
 const val TITLE_UI_PATH = "title"
 const val TITLE_SHAPE_PATH = "title_shape"
+const val RESPAWN_GROUND_DRAWABLE_PATH = "respawn_ground"
 
 //styles
 const val BUTTON_LEFT_STYLE = "button_left"
--- core/src/main/kotlin/com/goncharov/evgeny/tetris/screens/game/ui/GameScene.kt
+++ core/src/main/kotlin/com/goncharov/evgeny/tetris/screens/game/ui/GameScene.kt
@@ -2,12 +2,15 @@ package com.goncharov.evgeny.tetris.screens.game.ui
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.ui.Image
 import com.badlogic.gdx.scenes.scene2d.ui.Skin
 import com.badlogic.gdx.scenes.scene2d.ui.Table
+import com.badlogic.gdx.utils.Align
 import com.badlogic.gdx.utils.viewport.Viewport
 import com.goncharov.evgeny.tetris.custom.actors.MainBackground
 import com.goncharov.evgeny.tetris.navigation.Navigator
 import com.goncharov.evgeny.tetris.resources.BACKGROUND_LINE_UI_PATH
+import com.goncharov.evgeny.tetris.resources.RESPAWN_GROUND_DRAWABLE_PATH
 import com.goncharov.evgeny.tetris.resources.ResourceManager
 import com.goncharov.evgeny.tetris.resources.UI_ASSET_DESCRIPTOR
 import org.koin.core.component.KoinComponent
@@ -21,6 +24,7 @@ class GameScene(
 
     private val uiSkin: Skin = resourceManager[UI_ASSET_DESCRIPTOR]
     private val root = Table()
+    private val backgroundSpawnInfo = Image(uiSkin, RESPAWN_GROUND_DRAWABLE_PATH)
 
     init {
         initUi()
@@ -29,6 +33,8 @@ class GameScene(
     private fun initUi() {
         root.setFillParent(true)
         root.background(MainBackground(uiSkin.getSprite(BACKGROUND_LINE_UI_PATH)))
+        root.align(Align.right)
+        root.add(backgroundSpawnInfo).row()
         addActor(root)
     }
 }
