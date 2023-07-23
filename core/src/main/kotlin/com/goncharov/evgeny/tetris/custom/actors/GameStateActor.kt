d6273e3fb78ec73056765c7f4fbc98b22c54b958
Evgeny Goncharov <jtgn@yandex.ru>
2023-07-23 17:06:16 +0300

    added empty GameStateActor

---
+++ core/src/main/kotlin/com/goncharov/evgeny/tetris/custom/actors/GameStateActor.kt
@@ -0,0 +1,17 @@
+package com.goncharov.evgeny.tetris.custom.actors
+
+import com.badlogic.gdx.scenes.scene2d.ui.Skin
+import com.badlogic.gdx.scenes.scene2d.ui.Table
+
+class GameStateActor(
+    uiSkin: Skin
+) : Table() {
+
+    init {
+        initUi()
+    }
+
+    private fun initUi() {
+
+    }
+}
